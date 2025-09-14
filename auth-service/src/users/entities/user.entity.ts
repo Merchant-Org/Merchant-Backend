9865a5bdc0ebe9c2c3b25027ba69d9ffc2340ef6
2211846103 <m.ibrahim9276@gmail.com>
2025-09-14 17:51:33 +0200

    Added User Entity

--- auth-service/src/users/entities/user.entity.ts
+++ auth-service/src/users/entities/user.entity.ts
@@ -1,8 +1,9 @@
-import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+import { BaseEntity, Entity, PrimaryKey, Property } from '@mikro-orm/core';
+import * as bcrypt from 'bcrypt';
 
 
 @Entity()
-export class User {
+export class User extends BaseEntity {
   @PrimaryKey()
   id: string;
 
@@ -15,6 +16,18 @@ export class User {
   @Property()
   username: string;
 
+  @Property({ nullable: true })
+  email?: string;
+
+  @Property({ nullable: true })
+  phone?: string;
+
+  @Property({ nullable: true })
+  avatarUrl?: string;
+
+  @Property({ hidden: true })
+  password: string;
+
   @Property()
   createdAt: Date = new Date;
 
@@ -22,4 +35,12 @@ export class User {
     onUpdate: () => new Date(),
   })
   updatedAt: Date = new Date;
+
+  async setPassword(raw: string) {
+    this.password = await bcrypt.hash(raw, 10);
+  }
+
+  async checkPassword(raw: string) {
+    return bcrypt.compare(raw, this.password);
+  }
 }
