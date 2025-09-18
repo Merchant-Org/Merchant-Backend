efbb0f7730adbdab909b47124969201da1eb426c
IbrahimElsharif <Elsharif4458@outlook.com>
2025-09-18 20:19:06 +0200

    fixed subscribe method

--- src/store/tiers/entities/store-tier.entity.ts
+++ src/store/tiers/entities/store-tier.entity.ts
@@ -1,14 +1,16 @@
-import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+import { Entity, PrimaryKey, Property, ManyToOne } from '@mikro-orm/core';
 import { Tier } from './tier.entity';
+import { Store } from '../../entities/store.entity';
 
 @Entity()
 export class StoreTier {
     @PrimaryKey()
     id!: number;
 
-    @Property()
-    storeID!: number;
+    @ManyToOne(() => Store)
+    store!: Store;
 
-    @Property()
+    @ManyToOne(() => Tier)
     tier!: Tier;
+    
 }
--- src/store/tiers/tiers.service.ts
+++ src/store/tiers/tiers.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@mikro-orm/nestjs';
 import { EntityRepository } from '@mikro-orm/core';
 import { Tier } from './entities/tier.entity';
+import { StoreTier } from './entities/store-tier.entity';
+import { Store } from '../entities/store.entity';
 
 
 @Injectable()
@@ -9,16 +11,25 @@ export class TiersService {
     constructor(
         @InjectRepository(Tier)
         private readonly tierRepo: EntityRepository<Tier>,
+        @InjectRepository(Store)
+        private readonly storeRepo: EntityRepository<Store>,
+        @InjectRepository(StoreTier)
+        private readonly storeTierRepo: EntityRepository<StoreTier>,
+
     ) {}
 
     async getTiers() {
         return this.tierRepo.findAll();
     }
 
-    async createTiers(storeID: number, tierId: number) {
-        const tier = this.tierRepo.create({ store: { id: storeID }, tierId });
-        await this.tierRepo.getEntityManager().flush();
-        return tier;
+    async subscribe(storeID: number, tierID: number) {
+        const tier = await this.tierRepo.findOneOrFail(tierID);
+        const store = await this.storeRepo.findOneOrFail(storeID);
+
+        const sub = this.storeTierRepo.create({ store, tier });
+        await this.storeTierRepo.getEntityManager().flush();
+
+        return sub;
     }
 }
 
